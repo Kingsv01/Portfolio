9c612c50b906381a6e86e0bf5c71f6387e9a9faf
Kingsv01 <infosk530@gmail.com>
2023-04-17 13:02:06 +0300

    Portfolio Two

---
+++ Portfolio Two/Js/app.js
@@ -0,0 +1,9 @@
+//Heder toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  this.classList.toggle('fa-xmark')
+})
+
