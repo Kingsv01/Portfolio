e6759cf44c1433f781ede7fce8c2204902c08594
Kingsv01 <infosk530@gmail.com>
2023-04-21 10:03:16 +0300

    Portfolio-react One

--- Portfolio-react One/src/sections/services/Services.jsx
+++ Portfolio-react One/src/sections/services/Services.jsx
@@ -1,9 +1,27 @@
+import data from './deta'
+import Card from '../../component/Card'
 import './Services.css'
 
 const services = () => {
   return (
-    <section id="services"><h2>Services</h2></section>
-    )
+    <section id="services">
+    <h2>My Services</h2>
+    <p>I give you the best in oll services below</p>
+    <div className="container service__container">
+    {
+      data.map(item => (      
+        <Card key={item.id} className="service light">
+          <div className="service__icon">{item.icon}</div>
+          <div className="service__details">
+            <h4>{item.title}</h4>
+              <p>{item.desc}</p>
+          </div>
+        </Card>
+      ))
+    }
+    </div>
+    </section>
+  )
 }
 
 export default services
---
+++ Portfolio-react One/src/sections/services/deta.js
@@ -0,0 +1,21 @@
+import {SiAdobexd} from 'react-icons/si'
+import {RiReactjsLine} from 'react-icons/ri'
+import {FaServer} from 'react-icons/fa'
+import {AiFillAppstore} from 'react-icons/ai'
+
+const data = [
+  {
+    id: 1, icon: <SiAdobexd/>, title: 'Ui/Ux Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+  },
+  {
+    id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+  },
+  {
+    id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+  },
+  {
+    id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
+  }
+]
+
+export default data
